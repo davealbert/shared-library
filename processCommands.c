49a1183c571ce7c6c894adaaa25a8c85926df1e9
Dave Albert <dave.albert@publicisdhealthcare.ie>
2013-04-29 06:51:27 +0100

    ignore object files and correct string

--- processCommands.c
+++ processCommands.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 #define MAX_STRING_LEN 80
 
-void int processCommands() {
-  char commandInput[MAX_STRING_LEN];
-  commandInput = "This is a string";
+
+void processCommands() {
+  char *commandInput = "This is a string";
 
   printf("%s\n",commandInput );
-  return 0;
 }
